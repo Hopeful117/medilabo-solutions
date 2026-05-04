848718045480cb89d2979a747a67dabb411de788
ludo <ludovic.brot@gmail.com>
2026-05-04 11:58:44 +0200

    initial commit

---
+++ Patient-service/src/test/java/com/Medilabo_solutions/Patient_service/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Medilabo_solutions.Patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
