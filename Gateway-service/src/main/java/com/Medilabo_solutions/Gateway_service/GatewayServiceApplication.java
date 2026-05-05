369d0164d5ea144cfe447ebd755382d6a7317932
ludo <ludovic.brot@gmail.com>
2026-05-05 16:24:46 +0200

    init gateway service

---
+++ Gateway-service/src/main/java/com/Medilabo_solutions/Gateway_service/GatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Medilabo_solutions.Gateway_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+}
---
+++ Gateway-service/src/test/java/com/Medilabo_solutions/Gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Medilabo_solutions.Gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
