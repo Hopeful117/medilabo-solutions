40009057b42d66271178bea0da02cb7916f0e12c
ludo <ludovic.brot@gmail.com>
2026-05-06 09:42:53 +0200

    init frontend service

---
+++ Frontend-service/src/test/java/com/Medilabo_solutions/Frontend_service/FrontendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Medilabo_solutions.Frontend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
