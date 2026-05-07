59dfa04c87f8bcd97f86593edceb104dce99c0d1
ludo <ludovic.brot@gmail.com>
2026-05-07 11:23:44 +0200

    init security service

---
+++ Security-service/src/main/java/com/Medilabo_solutions/Security_service/SecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Medilabo_solutions.Security_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServiceApplication.class, args);
+	}
+
+}
---
+++ Security-service/src/test/java/com/Medilabo_solutions/Security_service/SecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Medilabo_solutions.Security_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
