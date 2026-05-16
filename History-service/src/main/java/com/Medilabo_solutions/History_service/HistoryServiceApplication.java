731ceda40e5fe4c3f7478b690ef772adbd93fc01
ludo <ludovic.brot@gmail.com>
2026-05-16 14:24:32 +0200

    initialisation history service

---
+++ History-service/src/main/java/com/Medilabo_solutions/History_service/HistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Medilabo_solutions.History_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoryServiceApplication.class, args);
+	}
+
+}
