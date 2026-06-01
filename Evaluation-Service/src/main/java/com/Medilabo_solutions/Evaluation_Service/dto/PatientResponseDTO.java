88414c1136091439aca42e76e6b1a81c082dee1f
Ludovic Brot <ludovic.brot@gmail.com>
2026-06-01 15:51:39 +0200

    ajout test IT Evaluation service

--- Evaluation-Service/src/main/java/com/Medilabo_solutions/Evaluation_Service/dto/PatientResponseDTO.java
+++ Evaluation-Service/src/main/java/com/Medilabo_solutions/Evaluation_Service/dto/PatientResponseDTO.java
@@ -1,10 +1,14 @@
 package com.Medilabo_solutions.Evaluation_Service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PatientResponseDTO {
     private long id;
     private String firstName;
